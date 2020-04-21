8b1dca6e1082570a81e62a44b6559b022c1a9833
vvasuki <vishvas.vasuki@gmail.com>
2020-04-21 20:19:43 +0530

    CU

---
+++ curation_utils/__init__.py
@@ -0,0 +1,4 @@
+# TODO: 
+#  Adapt https://github.com/rin2401/gdcmdtools3 and https://tanaikech.github.io/2017/05/02/ocr-using-google-drive-api/
+#  Move spreadsheet access stuff here.
+#  Move video-curation api helper.
