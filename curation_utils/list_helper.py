abba94f32d274c6338a388f212a0107ea22f47ee
vvasuki <vishvas.vasuki@gmail.com>
2020-04-25 06:53:21 +0530

    CU

---
+++ curation_utils/list_helper.py
@@ -0,0 +1,9 @@
+
+def split_into_n_bits(list_in, n):
+    k, m = divmod(len(list_in), n)
+    return (list_in[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+def divide_chunks(list_in, n):
+    # looping till length l 
+    for i in range(0, len(l), n):
+        yield l[i:i + n] 
