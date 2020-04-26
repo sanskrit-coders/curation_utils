c46fad97ace4e647dca69bb63d23badecd3cf842
vvasuki <vishvas.vasuki@gmail.com>
2020-04-26 12:38:49 +0530

    CU

--- curation_utils/file_helper.py
+++ curation_utils/file_helper.py
@@ -1,10 +1,11 @@
 import os
+import shutil
 
 
 def concatenate_files(input_path_list, output_path):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    with open(output_path, 'w') as outfile:
-        for fname in input_path_list:
-            with open(fname) as infile:
-                for line in infile:
-                    outfile.write(line)
+    with open(output_path,'wb') as outfile:
+        for f in input_path_list:
+            with open(f,'rb') as fd:
+                shutil.copyfileobj(fd, outfile)
+            outfile.write("\n".encode())
