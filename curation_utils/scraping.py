68de51ede115c8772daf1cbb2f9906491a501c22
vvasuki <vishvas.vasuki@gmail.com>
2020-05-26 16:33:43 +0530

    CU

--- curation_utils/scraping.py
+++ curation_utils/scraping.py
@@ -1,5 +1,17 @@
 import logging
 
+import requests
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s")
+
+# Set headers
+headers = requests.utils.default_headers()
+headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
+
 from selenium.webdriver.remote.remote_connection import LOGGER
 LOGGER.setLevel(logging.WARNING)
 
@@ -13,3 +25,6 @@ def get_selenium_browser(headless=True):
     opts = options.Options()
     opts.headless = headless
     return webdriver.Chrome(options=opts)
+
+def request_url(url):
+    return requests.get(url, headers)
