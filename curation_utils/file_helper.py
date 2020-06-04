def41e8500142c0cf5559ec2ec18d8d8d01ac888
drdhaval2785 <drdhaval2785@gmail.com>
2020-06-04 15:59:30 +0530

    https://github.com/sanskrit-coders/doc_curation/issues/11 closed

--- curation_utils/file_helper.py
+++ curation_utils/file_helper.py
@@ -3,10 +3,11 @@ import shutil
 
 
 def concatenate_files(input_path_list, output_path, add_newline_inbetween=False):
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    os.makedirs(os.path.dirname(os.path.abspath(output_path)), exist_ok=True)
     with open(output_path,'wb') as outfile:
         for f in input_path_list:
             with open(f,'rb') as fd:
                 shutil.copyfileobj(fd, outfile)
             if add_newline_inbetween:
                 outfile.write("\n".encode())
+
