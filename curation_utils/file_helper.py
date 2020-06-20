35e347487d661099926fac845050c1010c4f9ff9
vvasuki <vishvas.vasuki@gmail.com>
2020-06-20 21:29:38 +0530

    CU

--- curation_utils/file_helper.py
+++ curation_utils/file_helper.py
@@ -1,5 +1,9 @@
+import logging
 import os
 import shutil
+from pathlib import Path
+
+import regex
 
 
 def concatenate_files(input_path_list, output_path, add_newline_inbetween=False):
@@ -11,3 +15,19 @@ def concatenate_files(input_path_list, output_path, add_newline_inbetween=False)
             if add_newline_inbetween:
                 outfile.write("\n".encode())
 
+
+def clean_file_path(file_path):
+    return regex.sub("[^a-zA-Z0-9 _\\-~./]", "", file_path)
+
+
+def clean_file_names(dir_path, dry_run=False):
+    paths = list(Path(dir_path).glob("**/*"))
+    logging.info("Got %d paths", len(paths))
+    for path in paths:
+        path = str(path)
+        # logging.debug("Checking '%s'", path)
+        dest_path = clean_file_path(path)
+        if path != dest_path:
+            logging.info("Changing '%s' to '%s'", path, dest_path)
+            if not dry_run:
+                os.rename(path, dest_path)
